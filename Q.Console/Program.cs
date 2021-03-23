bcb6058b9e6894edc87f7fb337466646b2042351
Viktoria Nemkin <viktoria.nemkin@gmail.com>
2021-03-23 04:55:00 +0100

    Init project

---
+++ Q.Console/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Q.Console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
